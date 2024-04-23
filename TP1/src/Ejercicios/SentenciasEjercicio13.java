ca74622eef4630d1e3f1fec5e1205073e16d4b4d
Daniel D'Onofrio <danieldonofrio@live.com>
2024-04-23 18:50:00 -0300

    agrego ejercicio13

---
+++ TP1/src/Ejercicios/SentenciasEjercicio13.java
@@ -0,0 +1,11 @@
+package Ejercicios;
+
+public class SentenciasEjercicio13 {
+    public void mostrarnumeros()
+    {
+        for(int i=500; i>=200;i--)
+        {
+            System.out.print(i + " - ");    
+        }
+    }
+}
